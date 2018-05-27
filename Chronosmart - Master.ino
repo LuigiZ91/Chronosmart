5ff74d8dc208bd9329a8d27cabed4b4e32747872
LuigiZ91 <38993042+LuigiZ91@users.noreply.github.com>
2018-05-27 21:37:49 +0200

    Add files via upload

---
+++ Chronosmart - Master.ino
@@ -0,0 +1,102 @@
+#include <f401reMap.h>
+#include <SPI.h>   
+
+
+HardwareSerial mySerial(USART6);
+
+#define ledPin pinMap(12)
+#define BUTTON pinMap(38)
+
+const int triggerPort1 =  pinMap(10); 
+const int echoPort1 = pinMap(9); 
+const int led1 =  pinMap(12);
+int flagA = 0;
+int flagB = 0;
+int flagC = 0;
+int  val = 0; 
+
+int state = 0;
+int iniz = 0;
+int potValue = 0;
+void setup() {
+  pinMode(triggerPort1, OUTPUT);
+  pinMode(echoPort1, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  digitalWrite(ledPin, LOW);
+  mySerial.begin(9600); // Default communication rate of the Bluetooth module
+  Serial.begin(9600);
+}
+void loop() {
+  val = digitalRead(BUTTON);
+  
+  if(mySerial.available() > 0){ // Checks whether data is comming from the serial port
+    iniz = mySerial.read(); // Reads the data from the serial port
+  }
+//val==LOW 
+  if (iniz==1){
+    flagA=1;
+    flagB=0;
+    Serial.println("Starting...");
+    Serial.println("\n");
+  }
+
+  
+//flag che attiva il sistema tramite app, il sistema "vede"
+ if (flagA == 1) 
+  {  
+    
+   digitalWrite( triggerPort1, LOW );
+   digitalWrite( triggerPort1, HIGH );
+   delayMicroseconds( 10 );
+   digitalWrite( triggerPort1, LOW );
+   long durata1 = pulseIn(echoPort1, HIGH );
+Serial.print("distanza: ");
+        Serial.print(durata1 ,DEC);
+         Serial.print("\n");
+   
+   if (durata1 < 1500) // quando il sensore vede inizialmente 
+   {
+      Serial.println("I detect, when I stop detecting, the chronometer starts");
+      Serial.println("\n");
+       Serial.println(flagA);
+      Serial.println("\n");
+      Serial.println(flagB);
+      digitalWrite(led1, HIGH);
+      
+      while (flagB==0)
+      {
+        delayMicroseconds( 10000);
+        digitalWrite( triggerPort1, LOW );
+        digitalWrite( triggerPort1, HIGH );
+        delayMicroseconds( 10 );
+        digitalWrite( triggerPort1, LOW );
+        durata1 = pulseIn(echoPort1, HIGH );
+        Serial.print("distanza: ");
+        Serial.print(durata1 ,DEC);
+ Serial.print("\n");
+       
+        if (durata1 > 1500)
+        {
+          digitalWrite(led1, LOW);
+          mySerial.write(1);
+          state=1;
+          Serial.print("partito");
+          Serial.print("\n");
+          
+          flagB=1;
+          flagA=0;
+          iniz=0;
+        }
+           
+            
+      }
+   }
+  }
+  if(mySerial.available() > 0){ // Checks whether data is comming from the serial port
+    state = mySerial.read(); // Reads the data from the serial port
+     Serial.println("state:");
+    Serial.println(state);
+          } 
+ 
+}
